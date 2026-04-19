a029e62ac367f234aabb03fc92e7ed4c651ed579
vicente-hofmeister <vicentehofmeister@gmail.com>
2026-04-19 15:55:39 -0300

    Adding basic login and register logic

--- src/app/navigation/RootNavigator.tsx
+++ src/app/navigation/RootNavigator.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { LoginScreen } from "../screens/LoginScreen";
+import { LoginFormScreen } from "../screens/LoginFormScreen";
+import { RegisterScreen } from "../screens/RegisterScreen";
 import { HomeScreen } from "../screens/HomeScreen";
 
 export type RootStackParamList = {
   Login: undefined;
+  LoginForm: undefined;
+  Register: undefined;
   Home: undefined;
 };
 
@@ -14,6 +18,8 @@ export function RootNavigator() {
   return (
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Safe Travels" }} />
+      <Stack.Screen name="LoginForm" component={LoginFormScreen} options={{ title: "Entrar" }} />
+      <Stack.Screen name="Register" component={RegisterScreen} options={{ title: "Criar conta" }} />
       <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Home" }} />
     </Stack.Navigator>
   );
